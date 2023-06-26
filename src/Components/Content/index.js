a27c55279a4605962d11b8d54bee13077fc09361
Goliothjoe <s11910279@student.unklab.ac.id>
2023-06-26 22:26:42 +0800

    MID PROJECT CV

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -6,45 +6,44 @@ function Content() {
         <div>
             <p className='text-[#CA6D18] text-[30px] font-serif'>Hobby</p>
             <div className='mt-[10px]'>
-                <p className='text-[#222222] text-[30px] font-serif'>Automotive</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Bandung, April 2004 - Present</p>
+                <p className='text-[#222222] text-[30px] font-serif'>Sepak Bola</p>
+                <p className='text-[#797979] text-[25px] font-serif'>Sejak 2010 - Present</p>
             </div>
             <p className='w-[380px] text-[23px] font-thin mt-[12px] text-justify'>
-            Hobi otomotif saya melibatkan minat besar terhadap kendaraan dan teknologi otomotif. Saya menikmati mempelajari tentang mesin, sistem kelistrikan, serta melakukan perawatan rutin pada kendaraan pribadi. Saya juga senang mengikuti perkembangan industri otomotif dan terlibat dalam acara serta komunitas otomotif. Hobi ini mencerminkan keterampilan seperti ketelitian, pemecahan masalah, dan kerja tim, serta memicu keinginan untuk terus belajar dan mengembangkan pengetahuan dalam dunia otomotif.
+            Hobi saya bermain sepak bola telah menjadi bagian dari hidup saya sejak kecil. Saya menikmati permainan ini dengan sepenuh hati dan telah mengembangkan keterampilan serta semangat kompetitif melalui pengalaman bermain sepak bola sejak usia dini.
             </p>
             <div className='mt-[30px]'>
-                <p className='text-[#222222] text-[30px] font-serif'>Guitar</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Balikpapan, April 2011 - Present</p>
+                <p className='text-[#222222] text-[30px] font-serif'>Memasak</p>
             </div>
             <p className='w-[380px] text-[23px] font-thin mt-[12px] text-justify'>
-            Hobi bermain gitar telah menjadi kegemaran saya selama beberapa tahun. Saya memiliki pengetahuan dasar tentang teori musik, teknik bermain, dan pengertian akord serta skala. Saya sering meluangkan waktu untuk belajar lagu-lagu favorit dan mencoba menggabungkan berbagai gaya musik. Bermain gitar juga meningkatkan konsentrasi, ketekunan, dan disiplin saya..
+            Hobi saya dalam memasak adalah salah satu aspek yang mendalam dalam kehidupan saya. Saya menikmati menciptakan dan menggabungkan berbagai bahan makanan untuk menciptakan hidangan lezat. Saya telah mengembangkan keterampilan kulinari saya melalui eksperimen di dapur dan dengan antusiasme mencari resep baru. Hobi ini telah membantu saya menjadi kreatif, terorganisir, dan memiliki kemampuan multitugas yang baik.
             </p>
         </div>
         <div className=' w-full ml-[150px]'>
             <p className='text-[#CA6D18] text-[30px] font-serif'>Education & Learning</p>
             <div className='mt-[30px]'>
                 <p className='text-[#222222] text-[30px] font-serif'>TK</p>
-                <p className='text-[#797979] text-[25px] font-serif'>TK PERMATA BUNDA</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Balikpapan, 2006 - 2007</p>
+                <p className='text-[#797979] text-[25px] font-serif'>TK GMIM ELIM KOLONGAN</p>
+                <p className='text-[#797979] text-[25px] font-serif'>Tomohon, 2005 - 2006</p>
             </div>
             <div className='mt-[30px]'>
                 <p className='text-[#222222] text-[30px] font-serif'>Sekolah Dasar</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Adventist school</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Balikpapan, 2007 - 2013</p>
+                <p className='text-[#797979] text-[25px] font-serif'>SD GMIM 8 Tomohon</p>
+                <p className='text-[#797979] text-[25px] font-serif'>Tomohon, 2006 - 2012</p>
             </div>
             <div className='mt-[30px]'>
                 <p className='text-[#222222] text-[30px] font-serif'>Sekolah Menengah Pertama</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Adventist School </p>
-                <p className='text-[#797979] text-[25px] font-serif'>Balikpapan, 2013 - 2016</p>
+                <p className='text-[#797979] text-[25px] font-serif'>SMP NEGERI 1 TOMOHON </p>
+                <p className='text-[#797979] text-[25px] font-serif'>Tomohon, 2012 - 2015</p>
             </div>
             <div className='mt-[30px]'>
                 <p className='text-[#222222] text-[30px] font-serif'>Sekolah Menengah Atas</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Adventist School</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Balikpapan, 2016 - 2019</p>
+                <p className='text-[#797979] text-[25px] font-serif'>SMA KRISTEN 1 TOMOHON</p>
+                <p className='text-[#797979] text-[25px] font-serif'>Tomohon, 2015 - 2018</p>
             </div>
             <div className='mt-[30px]'>
                 <p className='text-[#222222] text-[30px] font-serif'>College</p>
-                <p className='text-[#797979] text-[25px] font-serif'>Klabat University</p>
+                <p className='text-[#797979] text-[25px] font-serif'>Universitas Klabat</p>
                 <p className='text-[#797979] text-[25px] font-serif'>Manado, 2019 - Present</p>
             </div>
         </div>
