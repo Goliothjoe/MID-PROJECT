b7391e91c5b8b59833b38732d6a9c1d57bf2160f
Jearly Putra <136541056+jearprox@users.noreply.github.com>
2023-06-26 16:40:17 +0800

    final

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -9,17 +9,17 @@ function Header() {
             <img src={Photo} alt="/" className='h-[150px] rounded-full' />
             <div className='ml-[20px]'>
                 <p className='text-[25px] font-serif text-[#CA6D18] mb-[10px] '>Unklab Student</p>
-                <p className='text-[38px] font-serif font-bold w-[50%] text-[#222222]'>Stephen Lobbu</p>
+                <p className='text-[38px] font-serif font-bold w-[50%] text-[#222222]'>Jearly Putra</p>
             </div>
         </div>
         <div className='relative mt-[36px] mr-[80px]'>
             <div className='flex flex-row items-center mb-[10px] '>
                 <img src={Mail} alt="/" className='h-[20px] mr-[10px]'/>
-                <p className=' underline text-[20px]'>timlobbu@gmail.com</p>
+                <p className=' underline text-[20px]'>Jearlyproxide@gmail.com</p>
             </div>
             <div className='flex flex-row items-center '>
                 <img src={Telephone} alt="/" className='h-[20px] mr-[10px]'/>
-                <p className=' underline text-[20px]'>+6281241512042</p>
+                <p className=' underline text-[20px]'>+6287874960022</p>
             </div>
         </div>
     </div>
